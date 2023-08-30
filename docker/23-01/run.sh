972ecaa4eb06cfb269f8597404fc8dc49c6b52ae
John Welsh <jwelsh@nvidia.com>
2023-08-30 11:17:58 -0700

    asdf

--- docker/23-01/run.sh
+++ docker/23-01/run.sh
@@ -8,5 +8,10 @@ docker run \
     --ipc host \
     --gpus all \
     --shm-size 14G \
+    --device /dev/video0:/dev/video0 \
+    -v /tmp/.X11-unix:/tmp/.X11-unix \
+    -e DISPLAY=$DISPLAY \
+    -p 5000:5000 \
+    -p 8888:8888 \
     -v $(pwd):/nanosam \
     nanosam:23-01
