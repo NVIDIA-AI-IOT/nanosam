2618fb2e9f9a950cd9e6ebb58ba17bfca0d21f33
John Welsh <jaybdub@users.noreply.github.com>
2023-09-06 10:17:14 -0700

    add coco eval

--- nanosam/tools/eval_coco.py
+++ nanosam/tools/eval_coco.py
@@ -1,30 +1,10 @@
-import pycocotools.mask as m
 import json
-import os
 from torchvision.datasets import CocoDetection
-import pycocotools.coco as coco
-import PIL.Image
 import numpy as np
 import matplotlib.pyplot as plt
 import tqdm
-from mobile_sam.predictor_trt import PredictorTRT
-
-dataset = CocoDetection(
-    root="data/coco_val/val2017",
-    annFile="data/coco_val/annotations/instances_val2017.json"
-)
-
-mobile_sam_predictor = PredictorTRT(
-    image_encoder_engine="data/mobile_sam_image_encoder.engine",
-    mask_decoder_engine="data/mobile_sam_mask_decoder.engine",
-    image_encoder_size=1024
-)
-
-nano_sam_predictor = PredictorTRT(
-    image_encoder_engine="data/resnet18_huber_1024_v5.engine",
-    mask_decoder_engine="data/mobile_sam_mask_decoder.engine",
-    image_encoder_size=1024
-)
+import argparse
+from nanosam.utils.predictor import Predictor
 
 
 def predict_box(predictor, image, box, set_image=True):
@@ -51,45 +31,70 @@ def predict_box(predictor, image, box, set_image=True):
 def box_xywh_to_xyxy(box):
     return [box[0], box[1], box[0] + box[2], box[1] + box[3]]
 
+
 def draw_box(box):
     x = [box[0], box[0], box[2], box[2], box[0]]
     y = [box[1], box[3], box[3], box[1], box[1]]
     plt.plot(x, y, 'g-')
-    
+
+
 def iou(mask_a, mask_b):
     intersection = np.count_nonzero(mask_a & mask_b)
     union = np.count_nonzero(mask_a | mask_b)
     return intersection / union
 
-ann = dataset[0][1][0]
 
-results = []
-for i in tqdm.tqdm(range(len(dataset))):
-    image, anns = dataset[i]
-    for j, ann in enumerate(anns):
-        id = ann['id']
-        area = ann['area']
-        category_id = ann['category_id']
-        iscrowd = ann['iscrowd']
-        image_id = ann['image_id']
-        box = box_xywh_to_xyxy(ann['bbox'])
-        mask = dataset.coco.annToMask(ann)
-        mask_coco = (mask > 0)
-        mask_nano_sam = predict_box(nano_sam_predictor, image, box, set_image=(j==0))
-        mask_mobile_sam = predict_box(mobile_sam_predictor, image, box, set_image=(j==0))
+if __name__ == "__main__":
 
-        result = {
-            "id": ann['id'],
-            "area": ann['area'],
-            "category_id": ann['category_id'],
-            "iscrowd": ann['iscrowd'],
-            "image_id": ann["image_id"],
-            "box": box,
-            "iou_nanosam_coco": iou(mask_nano_sam, mask_coco),
-            "iou_mobilesam_coco": iou(mask_mobile_sam, mask_coco),
-            "iou_mobilesam_nanosam": iou(mask_mobile_sam, mask_nano_sam)
-        }
-        results.append(result)
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--coco_root", type=str, default="data/coco/val2017")
+    parser.add_argument("--coco_ann", type=str, default="data/coco/instances_val2017.json")
+    parser.add_argument("--image_encoder", type=str, default="data/mobile_sam_image_encoder.engine")
+    parser.add_argument("--mask_decoder", type=str, default="data/mobile_sam_mask_decoder.engine")
+    parser.add_argument("--output", type=str, default="data/mobile_sam_coco_results.json")
+    args = parser.parse_args()
 
-with open("eval_coco_results.json", 'w') as f:
-    json.dump(results, f)
+        
+    dataset = CocoDetection(
+        root=args.coco_root,
+        annFile=args.coco_ann
+    )
+
+    predictor = Predictor(
+        image_encoder_engine=args.image_encoder,
+        mask_decoder_engine=args.mask_decoder,
+        image_encoder_size=1024
+    )
+
+    results = []
+
+    for i in tqdm.tqdm(range(len(dataset))):
+
+        image, anns = dataset[i]
+
+        for j, ann in enumerate(anns):
+
+            id = ann['id']
+            area = ann['area']
+            category_id = ann['category_id']
+            iscrowd = ann['iscrowd']
+            image_id = ann['image_id']
+            box = box_xywh_to_xyxy(ann['bbox'])
+            mask = dataset.coco.annToMask(ann)
+            mask_coco = (mask > 0)
+            mask_sam = predict_box(predictor, image, box, set_image=(j==0))
+
+            result = {
+                "id": ann['id'],
+                "area": ann['area'],
+                "category_id": ann['category_id'],
+                "iscrowd": ann['iscrowd'],
+                "image_id": ann["image_id"],
+                "box": box,
+                "iou": iou(mask_sam, mask_coco)
+            }
+
+            results.append(result)
+
+    with open(args.output, 'w') as f:
+        json.dump(results, f)
