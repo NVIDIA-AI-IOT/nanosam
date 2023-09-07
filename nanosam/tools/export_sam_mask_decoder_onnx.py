7882b0d58928132e11eb63f1f9a5fa3cf179abe7
John Welsh <jwelsh@nvidia.com>
2023-09-06 17:14:37 -0700

    add license

--- nanosam/tools/export_sam_mask_decoder_onnx.py
+++ nanosam/tools/export_sam_mask_decoder_onnx.py
@@ -1,6 +1,10 @@
+# Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+#   
+#   Adapted from https://github.com/ChaoningZhang/MobileSAM/blob/master/scripts/export_onnx_model.py
+#
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 # All rights reserved.
-
+#
 # This source code is licensed under the license found in the
 # LICENSE file in the root directory of this source tree.
 
--- setup.py
+++ setup.py
@@ -1,18 +1,24 @@
-# Copyright (c) Meta Platforms, Inc. and affiliates.
-# All rights reserved.
+# SPDX-FileCopyrightText: Copyright (c) 2023 NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+# SPDX-License-Identifier: Apache-2.0
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
 
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
 
 from setuptools import find_packages, setup
 
 setup(
     name="nanosam",
-    version="1.0",
+    version="0.0",
     install_requires=[],
-    packages=find_packages(exclude="notebooks"),
-    extras_require={
-        "all": ["matplotlib", "pycocotools", "opencv-python", "onnx", "onnxruntime"],
-        "dev": ["flake8", "isort", "black", "mypy"],
-    },
+    packages=find_packages()
 )
